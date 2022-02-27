14e23b712e65bf1906a3102b5293039842242cb8
AbhijitSKshirsagar <abhijitkshirsagar793@gmail.com>
2022-02-27 23:28:45 +0530

    added: linked list

--- src/com/BridgeLabz/CountNode.java
+++ src/com/BridgeLabz/CountNode.java
@@ -1,7 +1,6 @@
 package com.BridgeLabz;
 
 public class CountNode {
-
 	class Node {
 		int data;
 		Node next;
@@ -17,16 +16,23 @@ public class CountNode {
 	public Node tail = null;
 
 	// addNode() will add a new node to the list
-	/*
-	 * public void addNode(int data) { // Create a new node Node newNode = new
-	 * Node(data); // IDNode
-	 * 
-	 * // Checks if the list is empty if (head == null) { // If list is empty, both
-	 * head and tail will point to new node head = newNode; tail = newNode; } else {
-	 * // newNode will be added after tail such that tail's next will point to
-	 * newNode tail.next = newNode; // newNode will become new tail of the list tail
-	 * = newNode; } }
-	 */
+
+	public void addNode(int data) {
+		// Create a new node
+		Node newNode = new Node(data); // IDNode
+
+		// Checks if the list is empty
+		if (head == null) {
+			// If list is empty, both head and tail will point to new node
+			head = newNode;
+			tail = newNode;
+		} else {
+			// newNode will be added after tail such that tail's next will point to newNode
+			tail.next = newNode;
+			// newNode will become new tail of the list
+			tail = newNode;
+		}
+	}
 
 	public void append(int new_data) {
 		/*
@@ -57,6 +63,26 @@ public class CountNode {
 		return;
 	}
 
+	/* Inserts a new node after the given prev_node. */
+	public void insertAfter(Node prev_node, int new_data) {
+		/* 1. Check if the given Node is null */
+		if (prev_node == null) {
+			System.out.println("The given previous node cannot be null");
+			return;
+		}
+
+		/*
+		 * 2 & 3: Allocate the Node & Put in the data
+		 */
+		Node new_node = new Node(new_data);
+
+		/* 4. Make next of new Node as next of prev_node */
+		new_node.next = prev_node.next;
+
+		/* 5. make next of prev_node as new_node */
+		prev_node.next = new_node;
+	}
+
 	// display() will display all the nodes present in the list
 	public void display() {
 		// Node current will point to head
@@ -76,15 +102,16 @@ public class CountNode {
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Linked List");
+
 		CountNode sList = new CountNode();
 
 		// Add nodes to the list
-		sList.append(56);
-		sList.append(30);
+		sList.addNode(56);
 		sList.append(70);
+		sList.insertAfter(sList.head, 30);
 
 		// Displays the nodes present in the list
 		sList.display();
 	}
+
 }
